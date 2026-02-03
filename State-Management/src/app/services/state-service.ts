5024f5b94f1d5cfe3fe9f340183a69e4eab4a0fa
Noneeman <demkiv.sasha@gmail.com>
2026-02-03 11:33:47 +0000

    service with signals

--- State-Management/src/app/services/state-service.ts
+++ State-Management/src/app/services/state-service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal, Signal, WritableSignal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StateService {
+  private readonly _count = signal(0);
+
+  readonly Count: WritableSignal<number> = this._count;
+  readonly doubleCount: Signal<number> = computed(() => this._count() * 2);
   
+  increment() {
+    this._count.update((v) => v + 1);
+  }
+
+  decrement() {
+    this._count.update((v) => Math.max(0, v - 1));
+  }
+
+  getCount() {
+    return this._count();
+  }
 }
